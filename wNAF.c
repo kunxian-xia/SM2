0999e33ffda2595610f4ed2736f4babcc275ac24
kunxian xia <xiakunxian130@gmail.com>
2017-11-02 04:08:58 -0700

    详细分析scalar recode的原理

--- wNAF.c
+++ wNAF.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-//compute the wNAF of a big integer
 
+
+/*
+ * 原理介绍：
+ * 
+ * 在椭圆曲线群中，计算 -P 是很简单的；对于Weierstrass型曲线，若P = (x,y)，则-P = (x, -y)。
+ * 
+ * 因此，椭圆曲线点的多倍运算 k*P中，我们有必要要利用这种结构来减少预计算点的个数。
+ * 
+ * 比如，整数k = (1 00111 00010 11011)，二进制数k可以表达成如下的16进制数：
+ *          k = (1 7 3 -5)
+ * 
+ * 很明显，11011可以向上进1, 然后变成一个负数， 即11011 = 100000-100-1
+ * 于是，可以用ec_GFp_nistp_recode_scalar_bits函数来计算出。
+ */
+
+//in = a[5j+4] ... a[5j] a[5(j-1)+4]
 void ec_GFp_nistp_recode_scalar_bits(unsigned char *sign,
                                      unsigned char *digit, unsigned char in)
 {
@@ -9,14 +24,11 @@ void ec_GFp_nistp_recode_scalar_bits(unsigned char *sign,
     s = ~((in >> 5) - 1);       /* sets all bits to MSB(in), 'in' seen as
                                  * 6-bit value */
     d = (1 << 6) - in - 1;
-    d = (d & s) | (in & ~s);
-    //d = 0 0 0 in[5]^in[4] ... in[5]^in[0]
-    d = (d >> 1) + (d & 1);
-    /**
-     * d = 0 0 0 0 in[5]^in[4] ... in[5]^in[1]
-     *   + 0 0 0 0           0 ... in[5]^in[0]
-     *
-     */
+    d = (d & s) | (in & ~s);    /* d = (in[5]^in[4],...,in[5]^in[0]*/
+
+    d = (d >> 1) + (d & 1);     /* d = (in[5]^in[4],...,in[5]^in[1]) 
+                                 *                   + (in[5]^in[0]) */
+    
     *sign = s & 1;
     *digit = d;
 }
@@ -29,6 +41,7 @@ unsigned char get_bit(unsigned char *scalar, int i, int n)
         return (scalar[i >> 3] >> (i & 0x7)) & 1;
     }
 }
+
 //scalar is a n-bit integer
 void compute_wnaf(unsigned char *scalar, int n,
           unsigned char *sign, unsigned char *digit)
